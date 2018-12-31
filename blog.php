92b15bf01c46b8370532f106d2d1800201b1cea5
shadabsk <shaikhshadabali2@gmail.com>
2018-12-31 20:50:20 +0530

    added css and jquery fad in and out in login and blog.php

--- blog.php
+++ blog.php
@@ -1,37 +1,63 @@
 <!DOCTYPE html>
-<?php
-session_start();
+<?php session_start(); 
 /*
 * Author: Muhammed Salman Shamsi
 */?>
 <html>
-    <head>
-        <title>My Blog Page</title>
-        
-    </head>
-    <body>
-        <?php
-            if($_POST){
-                if($_POST['logout']=="yes"){
-                    session_destroy();
-                }
-            }
-            if(isset($_SESSION['user']))
-            {
-                echo "<h1>Welcome $_SESSION['user']!</h1>"
-                . "<b>College :</b> $_SESSION['college']<br>"
-                . "<b>Class : </b> $_SESSION['class']<br>"
-                . "<b>Batch : </b> $_SESSION['batch']<br>";
+<head>
+    <title>My Blog Page</title>
+</head>
+<style>
+    *{
+        text-align: center;
+        margin-top: 5%;
+        box-sizing: border-box; 
+    }
+    #brd{
+        border: 2px solid black;
+        border-radius: 25px 25px 25px 25px;
+        margin : 8%;
+        margin-left:20%;
+        margin-right:20%;
+        border-size: 
+    }
     
-                echo "<br>"
-                . "<form method='post' action='blog.php'>"
-                    . "<input type='hidden' name='logout' value='yes'/>" 
-                    . "<input type='submit' value='Logout'/>"
-                . "</form>";
+    input[type="submit"]{
+            width: 40%;
+            height: 40px;
+            background-color: #18C1A8;
+            color: #ffffff; 
+            border-radius: 4px;
+        }
+</style>
+<body>
+    <?php 
+        if($_POST)
+        {
+            if($_POST['logout']=="yes")
+            {
+                session_destroy();
             }
-            else{
-                header("Refresh: 0, url=login.php");
-            }
-        ?>
-    </body>
+        }
+        if(isset($_SESSION['user']))
+        {
+            echo "<div id=brd>"
+                ."<h1>Welcome $_SESSION[user]!<h1>"
+                ."<b>&emsp;&nbsp;College :</b> $_SESSION[college]<br>"
+                ."<b>Class :</b> $_SESSION[class]<br>"
+                ."<b>Batch :&nbsp;</b> $_SESSION[batch]<br>";
+
+            echo "<br>"
+                ."<form method='post' action='blog.php'>"
+                    ."<input type='hidden' name='logout' value='yes' />"
+                    ."<input type='submit' value='Logout' />"
+                ."</form>"
+                ."<div>";
+        }
+        else
+        {
+            header("Refresh:0,url=login.php");
+        }
+     ?>
+</body>
 </html>
--- login.php
+++ login.php
@@ -1,57 +1,98 @@
 <!DOCTYPE html>
 <?php session_start();?>
 <html>
-    <head>
-        <title>Login Page</title>
-        <style type="text/css">
-            #login-box{
-                width:25%;
-                margin: 7em auto;
-                border: 1px solid black;
-                border-radius: 5px;
-                padding: 10px;
-            }
-            #login-box>input{
-                display: block;
-                width:90%;
-                margin: 0 auto;
-                margin-bottom: 10px;
-            }
-            #login-box>span{
-                display: block;
-                text-align:center;
-                margin-bottom: 10px;
-            }
-        </style>
-    </head>
-    <body>
-        <?php
-            if($_POST){
-                $username=$_POST['username'];
-                $pass=$_POST['pass'];
-                if($username=="salman" && $pass=="12345")
-                {
-                    echo '<span>Login Successful! You will be redirected to blog page</span>';
-                    $_SESSION['user']=$username;
-                    $_SESSION['college']="AIKTC";
-                    $_SESSION['class']="TE";
-                    $_SESSION['batch']="B3";
-                    header("Refresh: 3, url=blog.php");
-                }
-                else{
-                    echo '<span>Incorrect username or password!</span>';
-                }
-            }
-        ?>
-        <form method="post" action="login.php">
-        <div id="login-box">
-            <span>Log In</span>
-            <input type="text" name="username" id="username" 
-                   placeholder="Username" required="required"/>
-            <input type="password" name="pass" id="pass" 
-                   placeholder="Password" required="required"/>
-            <input type="submit" value="Log In"/>
-        </div>
-        </form>
-    </body>
+<head>
+	<title>Login Page</title>
+	<style type="text/css">
+		#login-box{
+			width: 25%;
+			margin: 0 auto;
+			margin-top: 200px;
+			border: 1px solid black;
+			border-radius: 5px;
+			padding: 20px;
+			height: 120pt;
+		}
+		#login-box>input{
+			display: block;
+			width: 90%;
+			margin: 0 auto;
+			height: 20pt;
+			margin-bottom: 10px;
+			border-radius: 4px 4px 4px 4px;
+		}
+		#login-box>input[type="submit"]{
+			width: 40%;
+			height:	40px;
+			background-color: #18C1A8;
+			color: #ffffff; 
+			border-radius: 4px;
+		}
+		span{
+			display: block;
+			text-align: center;
+			margin-bottom: 10px;
+		}
+		.error{
+			position: fixed;
+			display: block;
+			text-align: center;
+			margin-top: 13%;
+			margin-left: 42%;
+		}
+		.suc{
+			position: fixed;
+			display: block;
+			text-align: center;
+			margin-top: 13%;
+			margin-left: 37%;
+		}
+	</style>
+</head>
+
+<body>
+	<?php
+		if ($_POST)
+		{
+			$username=$_POST['username'];
+			$pass=$_POST['pass'];
+			if($username=="salman" && $pass=="12345")
+			{
+				echo '<div class="suc" id=div1>Login Successful!You Will be redirected to blog page</div>';
+				$_SESSION['user']=$username;
+				$_SESSION['college']="AIKTC";
+				$_SESSION['class']="TE";
+				$_SESSION['batch']="B4";
+				header("Refresh:3,url=blog.php");
+			}
+			else
+			{
+				echo '<div class="error" id=div1> Incorrect username or password!</div>';
+				
+			}
+		}
+	?>
+	<form method="post" action="login.php">
+		<div id="login-box">
+			<span>Log In</span>
+			<input type="text" name="username" id="username" placeholder="Username" required="required" />
+			<input type="password" name="pass" id="pass" placeholder="Passsword" required="required" />
+			<input type="submit" id="submit" value="Log In" />
+		</div>
+	</form>
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	<script>
+		$(document).ready(function() { 
+			setTimeout(function() { 
+				$('#div1').fadeOut(); 
+		 }, 2000); 
+		});
+		$(document).ready(function() { 
+			setTimeout(function() { 
+				$('#div1').fadeOut(); 
+		 }, 2000); 
+		});
+
+	</script>
+</body>
 </html>
